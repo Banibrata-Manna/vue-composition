843c567f02186b0c5685cdd8c1924f56e96bcdd9
Banibrata Manna <banibratamanna@commons-MacBook-Air.local>
2025-08-31 19:31:11 +0530

    Vue Coposition API basics and ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p ref="para"> Hello My Name is {{ name }} and age is {{ age }}</p>
+    <button @click="handleClick">Click Me</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue';
+
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  mounted() {
+    // Logic to run when the component is mounted
+  },
+  created() {
+    // Logic to run when the component is created
+  },
+  setup() {
+    // Composition API logic can be added here if needed, always first
+
+     const para = ref(null);
+
+
+    // valuse defined here are not reactive as in data() { } property.
+    let name = 'Banibrata Manna';
+    let age = 25;
+
+    const handleClick = () => {
+      if (para.value) {
+        para.value.style.color = 'red';
+        para.value.style.fontSize = '30px';
+        para.value.style.fontWeight = 'bold';
+        para.value.textContent = 'Button Clicked. Name is Sonu and age is 24';
+      }
+    };
+
+    return {
+      // name: name,
+      // age: age
+      name, age, handleClick,
+      para
+    };
   }
 }
 </script>
